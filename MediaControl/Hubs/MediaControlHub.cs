b3cb9ef5a6c570a006966cbf1cd5fff07ffde9fe
seba2019 <mrsavl1@gmail.com>
2022-11-28 23:42:33 -0300

    commit inicial

---
+++ MediaControl/Hubs/MediaControlHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaControl.Hubs
+{
+    public class MediaControlHub : Hub
+    {
+        public async Task SendAction(string mediaAction)
+        {
+            await Clients.Others.SendAsync("MediaAction", mediaAction);
+        }
+    }
+}
---
+++ Services/Enum/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Enum
+{
+    enum eMediaAction
+    {
+        Play = 1,
+        Pause,Stop,NextSong,PrevSong,UpVol,DownVol
+    }
+}
